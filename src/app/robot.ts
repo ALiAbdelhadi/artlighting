84a861e0b984687218616e1417851641f8742f59
Ali Abdelhadi <aliabdelhadi010@gmail.com>
2024-10-17 15:04:55 +0300

    update project

--- src/app/robot.ts
+++ src/app/robot.ts
@@ -11,7 +11,6 @@ export default function robots(): MetadataRoute.Robots {
                 '/preview/'
             ]
         },
-        sitemap: 'https://eg-artlighting.vercel.app/sitemap.xml',
         host: 'https://eg-artlighting.vercel.app'
     }
 }
